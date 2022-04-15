7d87b5207551c5f246b9501e18ead114914f9329
Bob Provencher <bob@provencher.org>
2022-04-15 14:02:24 -0400

    first test

--- FizzBuzzKata/FizzBuzzKata.cs
+++ FizzBuzzKata/FizzBuzzKata.cs
@@ -1,8 +1,27 @@
-﻿namespace FizzBuzzKata
+﻿using System.Text;
+
+namespace FizzBuzzKata
 {
 
-	public class FizzBuzzKata
+	public static class FizzBuzz
 	{
+
+		public static string GetOutput() => GetOutput( 1, 100 );
+
+		public static string GetOutput( int start, int end )
+		{
+			var stringBuilder = new StringBuilder();
+			for ( int i = start; i <= end; i++ )
+			{
+				if ( stringBuilder.Length > 0 )
+				{
+					stringBuilder.Append( Environment.NewLine );
+				}
+				stringBuilder.Append( i );
+			}
+			return stringBuilder.ToString();
+		}
+
 	}
 
 }
